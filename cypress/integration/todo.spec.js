d7cfa61b38e432234447651880582ca40a6ba39e
Zharet Bautista Montes <zd.bautista@uniandes.edu.co>
2021-11-05 23:05:22 -0500

    Terminado

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -21,4 +21,19 @@ describe('TODOS page', () => {
       .click()
       .should('have.class', 'complete');
   });
+
+  it('should delete a previous TODO', () => {
+    cy.visit('/todo')
+      .get('input')
+      .type('new TODO')
+      .get('button[type=submit]')
+      .click()
+      .get('.list-group > .list-group-item:first > p')
+      .first()
+      .get('.btn.btn-danger')
+      .click()
+      .visit('/todo')
+      .get('.col-7>ul')
+      .should('be.empty');
+  });
 });
